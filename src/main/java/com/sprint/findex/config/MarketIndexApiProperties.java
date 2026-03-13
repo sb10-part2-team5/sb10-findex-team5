baa3f77d7b5b8617534bd2d19e9628d49b234637
Juuu00 <sungjuhyun1001@gmail.com>
2026-03-13 14:39:24 +0900

    refactor: baseUrl, serviceKey 검증 추가 및 MarketIndexApiClient 코드 개선

--- src/main/java/com/sprint/findex/config/MarketIndexApiProperties.java
+++ src/main/java/com/sprint/findex/config/MarketIndexApiProperties.java
@@ -1,10 +1,16 @@
 package com.sprint.findex.config;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app.external-api")
 public record MarketIndexApiProperties(
+    // 예외 메시지 필요
+    @NotBlank(message = "")
     String baseUrl,
+
+    // 예외 메시지 필요
+    @NotBlank(message = "")
     String serviceKey
 ) {
 
