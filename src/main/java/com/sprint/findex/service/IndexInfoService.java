186cd84d1e7fb42b69514f09add147fff9f4a3a1
Minbro-Kim <sojangjangi@gmail.com>
2026-03-13 19:20:14 +0900

    feat: 지수 정보 수정 기능 추가

--- src/main/java/com/sprint/findex/service/IndexInfoService.java
+++ src/main/java/com/sprint/findex/service/IndexInfoService.java
@@ -2,12 +2,15 @@ package com.sprint.findex.service;
 
 import com.sprint.findex.dto.indexinfo.IndexInfoCreateRequest;
 import com.sprint.findex.dto.indexinfo.IndexInfoDto;
+import com.sprint.findex.dto.indexinfo.IndexInfoUpdateRequest;
 import com.sprint.findex.entity.IndexInfo;
 import com.sprint.findex.enums.SourceType;
 import com.sprint.findex.exception.BusinessLogicException;
 import com.sprint.findex.exception.ExceptionCode;
 import com.sprint.findex.mapper.IndexInfoMapper;
 import com.sprint.findex.repository.IndexInfoRepository;
+import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,17 +20,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class IndexInfoService {
 
-    private final IndexInfoRepository indexInfoRepository;
-    private final IndexInfoMapper indexInfoMapper;
-    private final AutoSyncConfigService autoSyncConfigService;
+  private final IndexInfoRepository indexInfoRepository;
+  private final IndexInfoMapper indexInfoMapper;
+  private final AutoSyncConfigService autoSyncConfigService;
 
-    public IndexInfoDto createIndexInfoByUser(IndexInfoCreateRequest request) {
-        validateDuplicateIndexInfo(request);
-        IndexInfo indexInfo = createEntity(request, SourceType.USER);
-        indexInfoRepository.save(indexInfo);
-        autoSyncConfigService.createAutoSyncConfig(indexInfo);
-        return indexInfoMapper.toDto(indexInfo);
-    }
+  public IndexInfoDto createIndexInfoByUser(IndexInfoCreateRequest request) {
+    validateDuplicateIndexInfo(request);
+    IndexInfo indexInfo = createEntity(request, SourceType.USER);
+    indexInfoRepository.save(indexInfo);
+    autoSyncConfigService.createAutoSyncConfig(indexInfo);
+    return indexInfoMapper.toDto(indexInfo);
+  }
 
   /* openApi 지수정보를 생성 및 저장
   public IndexInfoDto createIndexInfoByOpenAPI(IndexInfoCreateRequest request) {
@@ -40,19 +43,27 @@ public class IndexInfoService {
   }
    */
 
-    private void validateDuplicateIndexInfo(IndexInfoCreateRequest request) {
-        if (indexInfoRepository.existsByIndexClassificationAndIndexName(
-                request.indexClassification(),
-                request.indexName())) {
-            throw new BusinessLogicException(ExceptionCode.INDEX_INFO_ALREADY_EXISTS);
-        }
-    }
+  public IndexInfoDto updateIndexInfoByUser(UUID id, IndexInfoUpdateRequest request) {
+    IndexInfo indexInfo = indexInfoRepository.findById(id)
+        .orElseThrow(() -> new BusinessLogicException(ExceptionCode.INDEX_INFO_NOT_FOUND));
+    indexInfo.updateIndexInfo(request.employedItemsCount(), request.basePointInTime(),
+        request.baseIndex(), request.favorite());
+    return indexInfoMapper.toDto(indexInfo);
+  }
 
-    private IndexInfo createEntity(IndexInfoCreateRequest request, SourceType sourceType) {
-        return IndexInfo.create(request.indexName(), request.indexClassification(),
-                request.employedItemsCount(), request.basePointInTime(), request.baseIndex(),
-                sourceType,
-                request.favorite());
-
+  private void validateDuplicateIndexInfo(IndexInfoCreateRequest request) {
+    if (indexInfoRepository.existsByIndexClassificationAndIndexName(
+        request.indexClassification(),
+        request.indexName())) {
+      throw new BusinessLogicException(ExceptionCode.INDEX_INFO_ALREADY_EXISTS);
     }
+  }
+
+  private IndexInfo createEntity(IndexInfoCreateRequest request, SourceType sourceType) {
+    return IndexInfo.create(request.indexName(), request.indexClassification(),
+        request.employedItemsCount(), request.basePointInTime(), request.baseIndex(),
+        sourceType,
+        request.favorite());
+
+  }
 }
