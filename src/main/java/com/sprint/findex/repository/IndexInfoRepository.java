3319219738f6a7d5415c4b0a6064a4ded75437f5
jh9dev <sungjuhyun1001@gmail.com>
2026-03-15 03:36:52 +0900

    feat: Open API 기반 IndexInfo 연동 서비스 및 조회 메서드 구현

--- src/main/java/com/sprint/findex/repository/IndexInfoRepository.java
+++ src/main/java/com/sprint/findex/repository/IndexInfoRepository.java
@@ -1,10 +1,14 @@
 package com.sprint.findex.repository;
 
 import com.sprint.findex.entity.IndexInfo;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IndexInfoRepository extends JpaRepository<IndexInfo, UUID> {
 
-    boolean existsByIndexClassificationAndIndexName(String indexClassification, String IndexName);
+  boolean existsByIndexClassificationAndIndexName(String indexClassification, String IndexName);
+
+  Optional<IndexInfo> findByIndexClassificationAndIndexName(String indexClassification,
+      String indexName);
 }
