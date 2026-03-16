8de32972451e853cf8d362900d9b4f84bb6a9fdb
Minbro-Kim <sojangjangi@gmail.com>
2026-03-16 16:36:03 +0900

    refactor: 스웨거 오타 수정

--- src/main/java/com/sprint/findex/dto/indexinfo/IndexInfoSummaryDto.java
+++ src/main/java/com/sprint/findex/dto/indexinfo/IndexInfoSummaryDto.java
@@ -9,7 +9,7 @@ public record IndexInfoSummaryDto(
     @Schema(description = "지수 정보 ID")
     UUID id,
 
-    @Schema(description = "지수 분류명", example = "KPSPI시리즈")
+    @Schema(description = "지수 분류명", example = "KOSPI시리즈")
     String indexClassification,
 
     @Schema(description = "지수명", example = "IT 서비스")
