9a1792f53c0a583321476faa28147290eac3f96b
Minbro-Kim <sojangjangi@gmail.com>
2026-03-18 17:14:39 +0900

    refactor: 연동작업은 로깅 시, 별도의 임계 값 설정

--- src/main/java/com/sprint/findex/aop/TimeTraceAspect.java
+++ src/main/java/com/sprint/findex/aop/TimeTraceAspect.java
@@ -3,6 +3,7 @@ package com.sprint.findex.aop;
 import com.sprint.findex.exception.BusinessLogicException;
 import jakarta.validation.ConstraintViolationException;
 import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -15,12 +16,20 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 @Component
 public class TimeTraceAspect {
 
+  public static List<String> WHITE_LIST = List.of("syncIndexData", "syncIndexInfo");
+
   @Around(" execution(* com.sprint.findex.controller..*(..))")
   //서비스 레벨로 찍으려면 아래 문장을 추가해주세요
   //|| execution(* com.sprint.findex.service..*(..))
   public Object trace(ProceedingJoinPoint joinPoint) throws Throwable {
     long start = System.currentTimeMillis();
     boolean success = true;
+
+    String taskName = joinPoint.getSignature().toShortString();
+    String methodName = joinPoint.getSignature().getName();
+
+    //연동 작업은 별도의 임계값 설정
+    long threshold = WHITE_LIST.contains(methodName) ? 500L : 150L;
 
     try {
       return joinPoint.proceed();
@@ -32,7 +41,7 @@ public class TimeTraceAspect {
       if (params.length() > 100) {
         params = params.substring(0, 100) + "...";
       }
-      String taskName = joinPoint.getSignature().toShortString();
+
 
       if (//유효성검사 및 비즈니스 에러는 경고
           throwable instanceof MethodArgumentNotValidException ||
@@ -46,10 +55,9 @@ public class TimeTraceAspect {
     } finally {
       long end = System.currentTimeMillis();
       long time = end - start;
-      String taskName = joinPoint.getSignature().toShortString();
 
       if (success) { //성공 로그
-        if (time > 100) {
+        if (time > threshold) {
           log.warn("[SLOW] {} | {}ms", taskName, time);
         } else {
           log.info("[OK] {} | {}ms", taskName, time);
