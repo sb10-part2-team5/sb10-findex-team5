4ad6c2d3e58c0a6e9543a1213d80c310feaf2a88
ParkSungGuk <cdjsdj1902@gmail.com>
2026-03-19 11:03:12 +0900

    fix: 배포 환경에서 지수데이터 export 에러 수정

--- src/main/java/com/sprint/findex/repository/dsl/IndexDataCustomRepository.java
+++ src/main/java/com/sprint/findex/repository/dsl/IndexDataCustomRepository.java
@@ -2,9 +2,16 @@ package com.sprint.findex.repository.dsl;
 
 import com.sprint.findex.dto.indexdata.IndexDataDto;
 import com.sprint.findex.dto.indexdata.IndexDataQueryCondition;
+import com.sprint.findex.entity.IndexData;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
+import org.springframework.data.domain.Sort;
 
 public interface IndexDataCustomRepository {
 
     List<IndexDataDto> findAllWithIndexDataQueryCondition(IndexDataQueryCondition condition);
+
+    List<IndexData> findAllForExport(UUID indexInfoId, LocalDate startDate, LocalDate endDate,
+            Sort sort);
 }
