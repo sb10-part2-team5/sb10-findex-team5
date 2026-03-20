3b4769bdee5db3ac4ce8b3aa8bf0fa5df0316346
Minbro-Kim <sojangjangi@gmail.com>
2026-03-20 09:37:04 +0900

    chore: swagger 서버 주소 변경

--- src/main/java/com/sprint/findex/config/OpenApiConfig.java
+++ src/main/java/com/sprint/findex/config/OpenApiConfig.java
@@ -3,11 +3,15 @@ package com.sprint.findex.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    @Value("${findex.server-url}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -17,8 +21,6 @@ public class OpenApiConfig {
                         .description("가볍고 빠른 외부 API 연동 금융 분석 도구 API 문서")
                 )
                 .addServersItem(new Server()
-                        .url("http://localhost:8080")
-                        .description("로컬 서버")
-                );
+                    .url(serverUrl).description("현재 환경 서버"));
     }
 }
